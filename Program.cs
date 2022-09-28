721fbe8a3f8e8bac935f9a0a3fdeab822a45a892
belakovacs958 <kovacsbela958@gmail.com>
2022-09-28 20:40:18 +0200

    url updated

--- Program.cs
+++ Program.cs
@@ -12,10 +12,12 @@ Console.WriteLine("write in a number");
 input = Console.ReadLine();
 
 
-var url = "http://172.24.9.102:4000/WeatherForecast/http%3A%2F%2F172.24.9.102%3A8080%2FWeatherForecast%2F" + input;
+var url = "http://192.168.1.103:4000/DistributeRequest/http%3A%2F%2F192.168.1.103%3A8080%2FListNumbers%2F" + input;
 
 HttpClient client = new HttpClient();
 string data = await client.GetStringAsync(url);
+
+
 
 System.Console.WriteLine(data);
 
